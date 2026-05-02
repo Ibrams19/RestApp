a7b297cd72d18eac6cf030796fe12865bbd7d5ae
ibrams19 <spydixix@gmail.com>
2026-05-02 15:13:31 +0000

    Fix: désactiver CSP Helmet pour scripts inline

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ const io = new Server(server, {
 });
 
 // ==================== CONFIGURATION ====================
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: false
+}));
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.static(__dirname));
